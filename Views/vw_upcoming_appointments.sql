a41be669337928d1260f141cc594ee8383a4ade7
Maklowicz-enjoyer <szymanskifilip46@gmail.com>
2025-06-29 21:05:27 +0200

    Update vw_upcoming_appointments.sql

--- Views/vw_upcoming_appointments.sql
+++ Views/vw_upcoming_appointments.sql
@@ -1,4 +1,5 @@
-CREATE VIEW v_upcoming_appointments AS
+-- Widok nadchodzących wizyt
+CREATE VIEW vw_upcoming_appointments AS
 SELECT 
     a.appointment_id,
     a.appointment_date,
@@ -9,10 +10,9 @@ SELECT
     e.user_id AS employee_id,
     CONCAT(emp.first_name, ' ', emp.last_name) AS employee_name,
     a.notes
-FROM appointments a
-JOIN users pat ON a.patient_id = pat.user_id
-JOIN patients p ON p.user_id = pat.user_id
-JOIN users emp ON a.employee_id = emp.user_id
-JOIN employees e ON e.user_id = emp.user_id
-WHERE a.status = 'scheduled' AND a.appointment_date >= NOW()
-ORDER BY a.appointment_date;
+FROM tbl_appointments a
+JOIN tbl_users pat ON a.patient_id = pat.user_id
+JOIN tbl_patients p ON p.user_id = pat.user_id
+JOIN tbl_users emp ON a.employee_id = emp.user_id
+JOIN tbl_employees e ON e.user_id = emp.user_id
+WHERE a.status = 'scheduled' AND a.appointment_date >= GETDATE();
