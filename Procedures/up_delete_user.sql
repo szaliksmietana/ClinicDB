28ac8e57c07295f20c850cc33d2aaf95a1dd3860
Maklowicz-enjoyer <szymanskifilip46@gmail.com>
2025-06-29 21:36:01 +0200

    Update up_delete_user.sql

--- Procedures/up_delete_user.sql
+++ Procedures/up_delete_user.sql
@@ -1,37 +1,10 @@
--- Procedura usuwania użytkownika
-CREATE PROCEDURE up_delete_user
-    @uid BIGINT
+-- Procedura resetowania hasła użytkownika
+CREATE PROCEDURE up_reset_user_password
+    @uid BIGINT,
+    @new_plain_password NVARCHAR(100)
 AS
 BEGIN
-    BEGIN TRY
-        BEGIN TRANSACTION
-
-        -- Usuń przypisane role
-        DELETE FROM tbl_user_roles WHERE user_id = @uid
-
-        -- Usuń dane typu (pracownik / pacjent)
-        DELETE FROM tbl_employees WHERE user_id = @uid
-        DELETE FROM tbl_patients WHERE user_id = @uid
-
-        -- Pobierz ID kontaktu i adresu przed usunięciem użytkownika
-        DECLARE @contact_id BIGINT, @address_id BIGINT
-        SELECT @contact_id = contact_id, @address_id = address_id 
-        FROM tbl_users WHERE user_id = @uid
-
-        -- Usuń użytkownika z tabeli głównej
-        DELETE FROM tbl_users WHERE user_id = @uid
-
-        -- Usuń dane kontaktowe i adresowe (jeśli istnieją)
-        IF @contact_id IS NOT NULL
-            DELETE FROM tbl_contacts WHERE contact_id = @contact_id
-        
-        IF @address_id IS NOT NULL
-            DELETE FROM tbl_addresses WHERE address_id = @address_id
-
-        COMMIT TRANSACTION
-    END TRY
-    BEGIN CATCH
-        ROLLBACK TRANSACTION
-        THROW
-    END CATCH
+    UPDATE tbl_users
+    SET password = CONVERT(NVARCHAR(64), HASHBYTES('SHA2_256', @new_plain_password), 2)
+    WHERE user_id = @uid
 END;
