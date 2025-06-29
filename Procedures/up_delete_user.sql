cee611bd7d6ea69df24ba421cb977ef4bd8c8e4f
Maklowicz-enjoyer <szymanskifilip46@gmail.com>
2025-06-29 21:20:28 +0200

    Update up_delete_user.sql

--- Procedures/up_delete_user.sql
+++ Procedures/up_delete_user.sql
@@ -1,28 +1,37 @@
--- Procedura: delete_user
--- Usuwa użytkownika wraz z powiązaniami: role, dane kontaktowe, adres, typ (employee/patient).
-
-DELIMITER //
-
-CREATE PROCEDURE delete_user(IN uid BIGINT UNSIGNED)
+-- Procedura usuwania użytkownika
+CREATE PROCEDURE up_delete_user
+    @uid BIGINT
+AS
 BEGIN
-    -- Usuń przypisane role
-    DELETE FROM user_roles WHERE user_id = uid;
+    BEGIN TRY
+        BEGIN TRANSACTION
 
-    -- Usuń dane typu (pracownik / pacjent)
-    DELETE FROM employees WHERE user_id = uid;
-    DELETE FROM patients WHERE user_id = uid;
+        -- Usuń przypisane role
+        DELETE FROM tbl_user_roles WHERE user_id = @uid
 
-    -- Usuń dane kontaktowe i adresowe (jeśli istnieją)
-    DELETE FROM contacts WHERE contact_id = (
-        SELECT contact_id FROM users WHERE user_id = uid
-    );
-    DELETE FROM addresses WHERE address_id = (
-        SELECT address_id FROM users WHERE user_id = uid
-    );
+        -- Usuń dane typu (pracownik / pacjent)
+        DELETE FROM tbl_employees WHERE user_id = @uid
+        DELETE FROM tbl_patients WHERE user_id = @uid
 
-    -- Usuń użytkownika z tabeli głównej
-    DELETE FROM users WHERE user_id = uid;
+        -- Pobierz ID kontaktu i adresu przed usunięciem użytkownika
+        DECLARE @contact_id BIGINT, @address_id BIGINT
+        SELECT @contact_id = contact_id, @address_id = address_id 
+        FROM tbl_users WHERE user_id = @uid
+
+        -- Usuń użytkownika z tabeli głównej
+        DELETE FROM tbl_users WHERE user_id = @uid
+
+        -- Usuń dane kontaktowe i adresowe (jeśli istnieją)
+        IF @contact_id IS NOT NULL
+            DELETE FROM tbl_contacts WHERE contact_id = @contact_id
+        
+        IF @address_id IS NOT NULL
+            DELETE FROM tbl_addresses WHERE address_id = @address_id
+
+        COMMIT TRANSACTION
+    END TRY
+    BEGIN CATCH
+        ROLLBACK TRANSACTION
+        THROW
+    END CATCH
 END;
-//
-
-DELIMITER ;
