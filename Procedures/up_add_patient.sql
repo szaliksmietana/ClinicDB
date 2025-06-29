f1176dc69a2f79b01403093061407de78c2d628e
Maklowicz-enjoyer <szymanskifilip46@gmail.com>
2025-06-29 21:18:14 +0200

    Update up_add_patient.sql

--- Procedures/up_add_patient.sql
+++ Procedures/up_add_patient.sql
@@ -1,42 +1,48 @@
-DELIMITER //
-
-CREATE PROCEDURE add_patient (
-    IN p_login VARCHAR(50),
-    IN p_password VARCHAR(255),
-    IN p_first_name VARCHAR(100),
-    IN p_last_name VARCHAR(100),
-    IN p_pesel VARCHAR(11),
-    IN p_birth_date DATE,
-    IN p_gender CHAR(1),
-    IN p_city VARCHAR(100),
-    IN p_postal_code VARCHAR(10),
-    IN p_street VARCHAR(255),
-    IN p_house_number VARCHAR(10),
-    IN p_apartment_number VARCHAR(10),
-    IN p_email VARCHAR(255),
-    IN p_phone CHAR(9),
-    IN p_insurance_number VARCHAR(50)
-)
+-- Procedura dodawania pacjenta
+CREATE PROCEDURE up_add_patient
+    @p_login NVARCHAR(50),
+    @p_password NVARCHAR(255),
+    @p_first_name NVARCHAR(100),
+    @p_last_name NVARCHAR(100),
+    @p_pesel NVARCHAR(11),
+    @p_birth_date DATE,
+    @p_gender NCHAR(1),
+    @p_city NVARCHAR(100),
+    @p_postal_code NVARCHAR(10),
+    @p_street NVARCHAR(255),
+    @p_house_number NVARCHAR(10),
+    @p_apartment_number NVARCHAR(10),
+    @p_email NVARCHAR(255),
+    @p_phone NCHAR(9),
+    @p_insurance_number NVARCHAR(50)
+AS
 BEGIN
-    DECLARE new_address_id INT;
-    DECLARE new_contact_id INT;
-    DECLARE new_user_id BIGINT;
+    DECLARE @new_address_id BIGINT
+    DECLARE @new_contact_id BIGINT
+    DECLARE @new_user_id BIGINT
 
-    INSERT INTO addresses (city, postal_code, street, house_number, apartment_number)
-    VALUES (p_city, p_postal_code, p_street, p_house_number, p_apartment_number)
-    RETURNING address_id INTO new_address_id;
+    BEGIN TRY
+        BEGIN TRANSACTION
 
-    INSERT INTO contacts (email, phone_number)
-    VALUES (p_email, p_phone)
-    RETURNING contact_id INTO new_contact_id;
+        INSERT INTO tbl_addresses (city, postal_code, street, house_number, apartment_number)
+        VALUES (@p_city, @p_postal_code, @p_street, @p_house_number, @p_apartment_number)
+        SET @new_address_id = SCOPE_IDENTITY()
 
-    INSERT INTO users (login, password, first_name, last_name, pesel, birth_date, gender, is_forgotten, address_id, contact_id)
-    VALUES (p_login, p_password, p_first_name, p_last_name, p_pesel, p_birth_date, p_gender, 0, new_address_id, new_contact_id)
-    RETURNING user_id INTO new_user_id;
+        INSERT INTO tbl_contacts (email, phone_number)
+        VALUES (@p_email, @p_phone)
+        SET @new_contact_id = SCOPE_IDENTITY()
 
-    INSERT INTO patients (user_id, insurance_number)
-    VALUES (new_user_id, p_insurance_number);
+        INSERT INTO tbl_users (login, password, first_name, last_name, pesel, birth_date, gender, is_forgotten, address_id, contact_id, access_level)
+        VALUES (@p_login, @p_password, @p_first_name, @p_last_name, @p_pesel, @p_birth_date, @p_gender, 0, @new_address_id, @new_contact_id, 1)
+        SET @new_user_id = SCOPE_IDENTITY()
+
+        INSERT INTO tbl_patients (user_id, insurance_number)
+        VALUES (@new_user_id, @p_insurance_number)
+
+        COMMIT TRANSACTION
+    END TRY
+    BEGIN CATCH
+        ROLLBACK TRANSACTION
+        THROW
+    END CATCH
 END;
-//
-
-DELIMITER ;
